a804cf7971fb3231a0a7e3a16ca018f3d899cfe8
guille <guille@users.noreply.github.com>
2025-10-07 10:37:15 +0200

    nvim: lsp actions on save and completions

--- nvim/autocmds.lua
+++ nvim/autocmds.lua
@@ -45,14 +45,38 @@ vim.api.nvim_create_autocmd('UIEnter', {
 
 -- When launching nvim without args: emulate "nvim ."
 vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    if vim.fn.argc() == 0 then
-      vim.schedule(function()
-        vim.cmd("e " .. vim.loop.cwd())
-      end)
-    end
-  end,
+	callback = function()
+		if vim.fn.argc() == 0 then
+			vim.schedule(function()
+				vim.cmd("e " .. vim.loop.cwd())
+			end)
+		end
+	end,
 })
+
+-- Use LSP for autocompletion when avaialble
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('my.lsp', {}),
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		-- LSP-powered completion
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+		-- LSP format on save
+		if not client:supports_method('textDocument/willSaveWaitUntil')
+		    and client:supports_method('textDocument/formatting') then
+			vim.api.nvim_create_autocmd('BufWritePre', {
+				group = vim.api.nvim_create_augroup('my.lsp', {clear=false}),
+				buffer = ev.buf,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
+				end,
+			})
+		end
+	end,
+})
+
 
 --
 -- Filetype-specific configs
