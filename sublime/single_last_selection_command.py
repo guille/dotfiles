0273eebf54411ec1ba6578d54f48cd6d7814e552
guille <guille@users.noreply.github.com>
2026-01-23 21:44:54 +0100

    sublime: scroll to last selection

--- sublime/single_last_selection_command.py
+++ sublime/single_last_selection_command.py
@@ -2,6 +2,7 @@ import sublime_plugin
 
 
 # https://superuser.com/questions/1050447/sublime-text-escape-multiple-selections-to-last-selection-region-instead-of-fir
+# Modified to make it scroll to the kept selection afterwards
 class SingleLastSelectionCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         view = self.view
@@ -9,3 +10,4 @@ class SingleLastSelectionCommand(sublime_plugin.TextCommand):
             last = view.sel()[-1]
             view.sel().clear()
             view.sel().add(last)
+            view.show(last)
