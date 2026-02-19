affaf7221fda73f053c40d8d9d19f0b3bd037d4e
guille <guille@users.noreply.github.com>
2026-02-19 15:45:35 +0100

    waybar: add idle_inhibitor + misc script improvements

--- waybar/scripts/dunst.sh
+++ waybar/scripts/dunst.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-if ! dunstctl is-paused --exit-code; then
+if dunstctl is-paused --exit-code; then
+	count=$(dunstctl count waiting)
+	disabled_text=" "
+	if [ "$count" != 0 ]; then
+		disabled_text=" $count"
+	fi
+	echo "$disabled_text"
+else
 	echo ""
-	exit
+	# echo " "
 fi
-
-count=$(dunstctl count waiting)
-disabled_text=" "
-if [ "$count" != 0 ]; then
-	disabled_text=" $count"
-fi
-echo "$disabled_text"
--- waybar/scripts/pacman.sh
+++ waybar/scripts/pacman.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if ! type pacman &> /dev/null; then
+	exit 1
+fi
+
 count=$(checkupdates | wc -l)
 if [ "$count" -ne 0 ]; then
 	echo "$count"
