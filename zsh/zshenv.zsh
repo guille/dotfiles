8fad7d3b0d84bf826ca8e53f7894c8eb93161263
guille <guille@users.noreply.github.com>
2026-06-20 13:47:17 +0200

    set CARGO_NET_GIT_FETCH_WITH_CLI=true

--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -16,6 +16,7 @@ export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 export RIPGREP_CONFIG_PATH="$HOME/.config/ripgrep/ripgreprc"
 
 export OPENCODE_DISABLE_LSP_DOWNLOAD=true
+export CARGO_NET_GIT_FETCH_WITH_CLI=true
 
 # export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
