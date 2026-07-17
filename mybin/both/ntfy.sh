a54059404695930cd5ec0e51243a2f6ad026ed6f
guille <guille@users.noreply.github.com>
2026-07-17 12:54:53 +0200

    ntfy-forward: configurable server

--- mybin/both/ntfy.sh
+++ mybin/both/ntfy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 SENTINEL="$XDG_RUNTIME_DIR/ntfy-forward"
+SERVER_FILE="$HOME/.config/ntfy-forward/server"
 TOPIC_FILE="$HOME/.config/ntfy-forward/topic"
 
 [[ -e "$SENTINEL" ]] || exit 0
@@ -8,4 +9,7 @@ TOPIC_FILE="$HOME/.config/ntfy-forward/topic"
 
 topic="$(<"$TOPIC_FILE")"
 
-curl -H "Title: ${DUNST_APP_NAME}" -d "$DUNST_SUMMARY" "https://ntfy.sh/${topic}"
+[[ -r "$SERVER_FILE" ]] && server="$(<"$SERVER_FILE")"
+server="${server:-ntfy.sh}"
+
+curl -H "Title: ${DUNST_APP_NAME}" -d "$DUNST_SUMMARY" "https://${server}/${topic}"
